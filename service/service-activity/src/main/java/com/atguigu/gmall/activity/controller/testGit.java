90f548e61cbc16c2464f73396b52139b96a95d16
feifan <15103200193@163.com>
2021-03-13 00:34:41 +0800

    测试

---
+++ service/service-activity/src/main/java/com/atguigu/gmall/activity/controller/testGit.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.activity.controller;
+
+public class testGit {
+
+    public void test1() {
+        System.err.println("分支添加");
+        System.err.println("主干添加");
+        System.err.println("更新1");
+    }
+}
